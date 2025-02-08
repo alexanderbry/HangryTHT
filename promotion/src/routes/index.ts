e545279db1f206a76f46c21ba41900f639114dc1
alexanderbry <alexunderbrain@gmail.com>
2025-02-08 21:46:32 +0700

    feat: authentication

--- promotion/src/routes/index.ts
+++ promotion/src/routes/index.ts
@@ -3,6 +3,7 @@ import errorHandler from "./middlewares/errorHandler";
 import PromotionController from "../controllers/promotionController";
 const router = Router();
 
+router.get("/", PromotionController.getApplicablePromotion);
 router.post("/create", PromotionController.createPromotion);
 
 router.use(errorHandler);
