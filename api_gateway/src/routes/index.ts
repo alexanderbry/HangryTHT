ae77a80fdfc858f93c96a46a5cc92a11acd089cd
alexanderbry <alexunderbrain@gmail.com>
2025-02-16 19:47:53 +0700

    refactor: adjust code with api gateway

--- api_gateway/src/routes/index.ts
+++ api_gateway/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { Request, Response } from "express";
 import dotenv from "dotenv";
+
 const router = Router();
 
 dotenv.config();
@@ -9,29 +10,49 @@ const userApp = process.env.USER_APP || "http://localhost:3000";
 const promotionApp = process.env.PROMOTION_APP || "http://localhost:3001";
 const orderApp = process.env.ORDER_APP || "http://localhost:3002";
 
-router.all("/:appName", async (req: Request, res: Response): Promise<any> => {
-  const app = req.params.appName;
-  const path = req.originalUrl;
+router.all(
+  "/:appName/*",
+  async (req: Request, res: Response): Promise<void> => {
+    const app = req.params.appName;
+    const path = req.originalUrl;
 
-  let targetUrl = "";
+    let headers: Record<string, string> = {};
+    if (req.headers.authorization) {
+      headers["Authorization"] = req.headers.authorization as string;
+      headers["Content-Type"] = "application/json";
+    } else {
+      headers["Content-Type"] = "application/json";
+    }
+    
+    let targetUrl = "";
 
-  switch (app) {
-    case "user":
-      targetUrl = `${userApp}${path}`;
-      break;
-    case "order":
-      targetUrl = `${orderApp}${path}`;
-      break;
-    case "promotion":
-      targetUrl = `${promotionApp}${path}`;
-      break;
-    default:
-      return res.status(404).json({ message: "Service not found" });
-  }
-  console.log(`Redirecting to ${targetUrl}`);
-  
+    switch (app) {
+      case "user":
+        targetUrl = `${userApp}${path}`;
+        break;
+      case "order":
+        targetUrl = `${orderApp}${path}`;
+        break;
+      case "promotion":
+        targetUrl = `${promotionApp}${path}`;
+        break;
+      default:
+        res.status(404).json({ message: "Service not found" });
+    }
 
-  return res.redirect(targetUrl);
+    try {
+      const response = await fetch(targetUrl, {
+        method: req.method,
+        headers: headers,
+        body: req.method === "GET" ? undefined : JSON.stringify(req.body),
+      });
+
+      const data = await response.json();
+      res.status(response.status).json(data);
+    } catch (error) {
+      console.error("Error making request to target service:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
 });
 
 export default router;
